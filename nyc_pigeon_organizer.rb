598c1f688ccc3f33bdb7f31c67279ee09a5334c3
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-08-22 01:22:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -28,7 +28,7 @@ def nyc_pigeon_organizer(data)
   #2.Iterate over the hash above, collecting each pigeon by name and insert it as the key of a new hash.
   #3. Each of these hashes should have :color, :gender, and :lives keys assigned to arrays of info about that particular pigeon. 
   pigeon_list = {}
-  data.each do |attribute|
+  data.each do |key, value|
     binding.pry
   end
   
